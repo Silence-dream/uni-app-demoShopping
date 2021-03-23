d240c7a539fe01fd5c2b95a821df43f7b26850e8
Silence-dream <1428482231@qq.com>
2021-03-24 07:59:48 +0800

    轮播图

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from "vue";
+import App from "./App";
 
-Vue.config.productionTip = false
+// 导入第三方包
+import { $http } from "@escook/request-miniprogram";
+uni.$http = $http
 
-App.mpType = 'app'
+// 配置请求根路径
+$http.baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1";
 
+// 请求开始之前做一些事情
+$http.beforeRequest = function () {
+  uni.showLoading({
+  	title:"加载中"
+  })
+};
+
+// 请求完成之后的事情
+$http.afterRequest = function () {
+  uni.hideLoading();
+};
+
+Vue.config.productionTip = false;
+
+App.mpType = "app";
 
 const app = new Vue({
-    ...App
-})
-app.$mount()
+  ...App,
+});
+app.$mount();
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,25 +1,46 @@
 <template>
-	<div>
-		<view>home123123</view>
-		<view>home123123</view>
-		<view>home123123</view>
-		<view>home123123</view>
-		<view>home123123</view>
-		<view>home123123</view>
-		<view>home123123</view>
-		<view>home123123</view>
-		<view>home123123</view>
-		<view>home123123</view>
-		<view>home123123</view>
+	<div id="home">
+		<swiper class="banner" indicator-dots autoplay>
+			<swiper-item v-for="(item,index) in swiperList" :key="index">
+				<image :src="item.image_src" mode="widthFix"></image>
+			</swiper-item>
+		</swiper>
 	</div>
 </template>
 
 <script>
 export default {
 	data() {
-		return {};
+		return {
+			swiperList:[]
+		};
+	},
+	onLoad() {
+		uni.$http.get("/home/swiperdata").then(data=>{
+			console.log(data);
+		})
+		this.getSwiper()
+	},
+	methods:{
+		/**
+		 * 获取轮播图图片
+		 */
+		async getSwiper(){
+			let { data } = await uni.$http.get("/home/swiperdata");
+			this.swiperList=data.message;
+			console.log(this.swiperList);
+		}
 	}
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+// 轮播图
+.banner{
+	width: 100%;
+	height: 340rpx;
+	image{
+		width: 100%;
+	}
+}
+</style>
