d96d6dea5fcb1f859c2ba025da894ba2470e85bd
Silence-dream <1428482231@qq.com>
2021-03-23 21:16:31 +0800

    项目初始化

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # uni-app-demoShopping
 uniapp版本的黑马优购
+
+[api 地址](https://www.showdoc.com.cn/128719739414963)
---
+++ pages/home/home.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<view>home123123</view>
+		<view>home123123</view>
+		<view>home123123</view>
+		<view>home123123</view>
+		<view>home123123</view>
+		<view>home123123</view>
+		<view>home123123</view>
+		<view>home123123</view>
+		<view>home123123</view>
+		<view>home123123</view>
+		<view>home123123</view>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="scss"></style>
