6950dc1ce9a410504bec6945e1c14b15d2e02953
Silence-dream <1428482231@qq.com>
2021-03-29 20:07:45 +0800

    搜索历史完成

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -7,7 +7,7 @@
       cancelButton="none"
     ></uni-search-bar>
     <!-- 搜索建议列表 -->
-    <view class="sugg-list">
+    <view class="sugg-list" v-if="searchResults.length !== 0">
       <view
         class="sugg-item"
         v-for="(item, i) in searchResults"
@@ -16,6 +16,22 @@
       >
         <view class="goods-name">{{ item.goods_name }}</view>
         <uni-icons type="arrowright" size="16"></uni-icons>
+      </view>
+    </view>
+    <!-- 搜索历史 -->
+    <view class="history-box" v-else>
+      <!-- 标题区域 -->
+      <view class="history-title">
+        <text>搜索历史</text>
+        <uni-icons type="trash" size="17"></uni-icons>
+      </view>
+      <!-- 列表区域 -->
+      <view class="history-list">
+        <uni-tag
+          :text="item"
+          v-for="(item, i) in historyList"
+          :key="i"
+        ></uni-tag>
       </view>
     </view>
   </view>
@@ -31,6 +47,8 @@ export default {
       kw: "",
       // 搜索结果列表
       searchResults: [],
+      // 搜索关键词的历史记录
+      historyList: [],
     };
   },
   methods: {
@@ -59,11 +77,23 @@ export default {
       });
       if (res.meta.status !== 200) return uni.$showMsg();
       this.searchResults = res.message;
+      // 1. 查询到搜索建议之后，调用 saveSearchHistory() 方法保存搜索关键词
+      this.saveSearchHistory();
     },
     gotoDetail(goods_id) {
       uni.navigateTo({
         url: `/subpkg/good_detail/good_detail?goods_id=${goods_id}`,
       });
+    },
+    // 2. 保存搜索关键词的方法
+    saveSearchHistory() {
+      // 2.1 直接把搜索关键词 push 到 historyList 数组中
+      this.historyList.push(this.kw);
+      // 翻转数组让后搜索在前面
+      this.historyList.reverse();
+      // 数组去重
+      let arr = new Set(this.historyList);
+      this.historyList = Array.from(arr);
     },
   },
 };
@@ -104,6 +134,28 @@ export default {
       // 文本溢出后，使用 ... 代替
       text-overflow: ellipsis;
       margin-right: 3px;
+    }
+  }
+}
+.history-box {
+  padding: 0 5px;
+
+  .history-title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 40px;
+    font-size: 13px;
+    border-bottom: 1px solid #efefef;
+  }
+
+  .history-list {
+    display: flex;
+    flex-wrap: wrap;
+
+    .uni-tag {
+      margin-top: 5px;
+      margin-right: 5px;
     }
   }
 }
